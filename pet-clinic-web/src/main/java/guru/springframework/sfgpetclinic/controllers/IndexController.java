2f619646deef7108ae3ac8c312650a712f423cb4
Nicolas Raigosa <nicolasraigosa@gmail.com>
2023-05-10 17:32:19 -0500

    Fixing Broken Links

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,11 @@ public class IndexController {
     public String index(){
 
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+
+        return "notimplemented.html";
     }
 }
